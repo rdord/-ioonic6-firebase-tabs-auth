a6de9f68073916401f1be8a5d6c44a17287ba5c3
Roland Dordevic <rdorde@gmail.com>
2023-01-28 23:16:12 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ioonic6-firebase-tabs-auth',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
